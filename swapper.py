7d936fe96808262767b0ceb28c592a11b9ebf48b
Grant Mercer <Gmercer015@gmail.com>
2016-05-04 19:11:04 -0700

    New lower frame for conversion

--- swapper.py
+++ swapper.py
@@ -6,35 +6,35 @@
 import constants
 
 from Tkconstants import RIGHT, END, DISABLED, BOTH, LEFT, \
-    VERTICAL, Y, GROOVE, RIDGE, SOLID
+    VERTICAL, Y, GROOVE, SUNKEN, SOLID, TOP, W, E, BOTTOM, X, \
+    CENTER
 from Tkinter import Tk, Label, Toplevel, Menu, PanedWindow, \
     Frame, Button, StringVar, Text
 from log import logger
+from PIL import ImageTk
 
 class ui:
     def __init__(self, r):
         self.__root = r         # root of program
+        self.__load_icon = ImageTk.PhotoImage(file=constants.PATH + r'/ico/load.png')
 
         # public window dimensions
         self.width = self.__root.winfo_screenwidth()
         self.height = self.__root.winfo_screenheight()
 
-        master_frame = Frame(self.__root, width=constants.WIDTH/3,
-                             borderwidth=1, relief=SOLID)
-        master_frame.pack(side=LEFT, fill=BOTH, expand=1, padx=5, pady=5)
-
-        sub_frame = Frame(self.__root, width=constants.WIDTH - constants.WIDTH/3,
-                          borderwidth=1, relief=SOLID)
-        sub_frame.pack(side=RIGHT, fill=BOTH, expand=1, padx=5, pady=5)
+        self.__master_frame = Frame(self.__root, width=constants.WIDTH/3, borderwidth=1, relief=SOLID,
+                                    height=constants.HEIGHT/4)
+        self.__master_frame.grid_propagate(False)
+        self.__master_frame.grid(row=0, column=0, padx=5, pady=5)
 
 
-        """
-        self.__master_db_frame = Frame(base_pane, bg="blue")
-        self.__master_db_frame.pack(side=LEFT)
+        self.__convert_frame = Frame(self.__root, width=constants.WIDTH/3, borderwidth=1, relief=SOLID,
+                                     height=constants.HEIGHT - constants.HEIGHT/4 - 20)
+        self.__convert_frame.grid_propagate(False)
+        self.__convert_frame.grid(row=1, column=0, padx=5, pady=5)
 
-        self.__sub_db_frames = Frame(base_pane, bg="green")
-        self.__master_db_frame.pack(side=RIGHT)
-        """
+        self.__sub_frame = Frame(self.__root, width=constants.WIDTH - constants.WIDTH/3, borderwidth=1, relief=SOLID)
+        self.__sub_frame.grid(row=0, column=1, padx=5, pady=5)
 
     def setup_gui(self):
         self.__root.title('db_swapper')
@@ -42,6 +42,24 @@ class ui:
         y = (self.height - constants.HEIGHT) / 2
 
         self.__root.geometry('%dx%d+%d+%d' % (constants.WIDTH, constants.HEIGHT, x, y))
+
+        load_btn = Button(self.__master_frame, image=self.__load_icon, width=20, height=20)
+        load_btn.grid(row=1, column=1, padx=5, pady=5)
+
+        lbl_master = Label(self.__master_frame, text="Master List")
+        lbl_master.grid(row=1, column=2, padx=5, pady=5)
+
+        lbl_file = Label(self.__master_frame, bg="white", relief=SUNKEN,
+                         width=26)
+        lbl_file.grid(row=2, column=1, padx=5, pady=5, columnspan=3)
+
+        self.__master_frame.grid_rowconfigure(0, weight=1)
+        self.__master_frame.grid_columnconfigure(3, weight=1)
+        self.__master_frame.grid_rowconfigure(3, weight=1)
+        self.__master_frame.grid_columnconfigure(0, weight=1)
+
+
+
 
 def main():
     logger.info('db_swapper v0.1 changelog')
